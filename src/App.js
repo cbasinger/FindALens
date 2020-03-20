ba00e940c341ccb6b663b73537584a6efffc48b0
Chelsea Basinger <chelsea.basinger@gmail.com>
2020-03-20 14:39:28 -0400

    Added redux components for users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -39,15 +39,31 @@ const {setCurrentUser} = this.props;
       <div>
         <Header  />
         <Switch>
-          <Route path='/signin' component={SignInandSignUpPage} />
+          <Route 
+          exact path='/signin' 
+          render={() => 
+            this.props.currentUser ? (
+            <Redirect to = '/' />
+            ) : (
+            <SignInandSignUpPage/>
+            )
+        } 
+        />
         </Switch>
       </div>
       );
     }
   }
+
+const mapStatetoProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
   
 const mapDispatchtoProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user)) 
 });
 
-export default connect(null, mapDispatchtoProps)(App);
+export default connect(
+  mapStatetoProps, 
+  mapDispatchtoProps)
+  (App);
