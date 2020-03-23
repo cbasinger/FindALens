9aaf8d2684bd2b52052c6fc091a7d1b2c7a9f901
salientype <jorge@salientype.com>
2020-03-23 17:36:58 -0400

    added reactstrap

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,42 +3,97 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { selectCurrentUser } from '../../redux/user/user.selector';
-import { selectCartHidden } from '../../redux/cart/cart.selectors'; 
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
 
 import './header.styles.scss';
 
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container,
+    Row,
+    Col,
+    Button
+} from 'reactstrap';
 
-const Header = ({ currentUser, hidden }) => (
-    <div className='header'>
-        <Link className= 'logo-container' to='/'>
-        FIND A LENS
-        </Link>
-        <div className='options'>
-            <Link className='option' to= '/contact'>
-                CONTACT
-            </Link>
-            <Link className='option' to='/profile' >
-                PROFILE
-            </Link>
-                {currentUser ? (
-                    <div className='option' onClick={() => auth.signOut()} > Sign Out
-                    </div>) 
-                    : (
-                    <Link className='option' to='/signin'> SIGN IN </Link>
-                    )}
-            <CartIcon />
-        </div>
-            {
-                hidden ? null : <CartDropdown />
-            }
-    </div>
-);
+
+
+class Header extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+
+    render() {
+
+        const currentUser = this.props.currentUser;
+        const hidden = this.props.hidden;
+
+        return (
+            <div className='header'>
+                <Navbar color="inverse" light expand="md">
+                    <Link className='logo-container' to='/'>
+                        <NavbarBrand href="/">FIND A LENS</NavbarBrand>
+                    </Link>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <div className='options'>
+                                <Link className='option' to='/contact'>
+                                    <NavItem>
+                                        CONTACT
+                                    </NavItem>
+                                </Link>
+                                <Link className='option' to='/profile' >
+                                    <NavItem>
+                                        PROFILE
+                            </NavItem>
+                                </Link>
+                                {currentUser ? (
+                                    <div className='option' onClick={() => auth.signOut()} > Sign Out
+                                    </div>)
+                                    : (
+                                        <Link className='option' to='/signin'>
+                                            <NavItem>
+                                                SIGN IN
+                                            </NavItem>
+                                        </Link>
+                                    )}
+                                <CartIcon />
+                            </div>
+                            {
+                                hidden ? null : <CartDropdown />
+                            }
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        )
+    }
+}
 
 const mapStateToProps = createStructuredSelector({
     currentUser: selectCurrentUser,
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import store from './redux/store';
 
 ReactDOM.render(
